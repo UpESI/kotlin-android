0d9499163416e6b56537b5e5c7b4a8fc60d84d24
Kevin Vavelin <kvavelin@geek-inc.fr>
2018-11-19 17:20:13 +0100

    Update

---
+++ MoodTracker/app/src/main/java/geekinc/fr/moodtracker/moods/MoodThumbs.kt
@@ -0,0 +1,13 @@
+package geekinc.fr.moodtracker.moods
+
+import geekinc.fr.moodtracker.R
+
+val moodThumbs = arrayOf(
+    R.drawable.angry,
+    R.drawable.blushed,
+    R.drawable.happy,
+    R.drawable.joy,
+    R.drawable.kiss,
+    R.drawable.lol,
+    R.drawable.tongue
+)
