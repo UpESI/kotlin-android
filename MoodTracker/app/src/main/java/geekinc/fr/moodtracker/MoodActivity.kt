1e30f54156d6f6a3a2ed6bff220df35b719d7557
Kevin Vavelin <kvavelin@geek-inc.fr>
2018-11-19 17:02:48 +0100

    Add activity, enum, and values for drawable

--- MoodTracker/app/src/main/java/geekinc/fr/moodtracker/MoodActivity.kt
+++ MoodTracker/app/src/main/java/geekinc/fr/moodtracker/MoodActivity.kt
@@ -2,9 +2,9 @@ package geekinc.fr.moodtracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
-import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+import geekinc.fr.moodtracker.extras.ExtraActions
+import geekinc.fr.moodtracker.moods.moodThumbs
 
 class MoodActivity : AppCompatActivity() {
 
@@ -12,39 +12,10 @@ class MoodActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mood)
 
-        val moodString = intent.getStringExtra("mood")
+        val moodIndex = intent.getIntExtra(ExtraActions.EXTRA_INT.toString(), 0)
 
-        val button = findViewById<Button>(R.id.mood_button).apply {
-            text = moodString
-        }
+        val moodImageView = findViewById<ImageView>(R.id.mood_image_view)
+        moodImageView.setImageResource(moodThumbs[moodIndex])
 
-        val rootView = button.rootView
-
-        when {
-            moodString == getString(R.string.mood_happy) -> {
-                findViewById<TextView>(R.id.mood_text_view).apply {
-                    text = getString(R.string.mood_happy_text)
-                }
-                rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.mood_happy))
-            }
-            moodString == getString(R.string.mood_cry) -> {
-                findViewById<TextView>(R.id.mood_text_view).apply {
-                    text = getString(R.string.mood_cry_text)
-                }
-                rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.mood_cry))
-            }
-            moodString == getString(R.string.mood_awesome) -> {
-                findViewById<TextView>(R.id.mood_text_view).apply {
-                    text = getString(R.string.mood_awesome_text)
-                }
-                rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.mood_awesome))
-            }
-            moodString == getString(R.string.mood_hurt) -> {
-                findViewById<TextView>(R.id.mood_text_view).apply {
-                    text = getString(R.string.mood_hurt_text)
-                }
-                rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.mood_hurt))
-            }
-        }
     }
 }
