03d42715e42160495395994afb25415500f1b0e0
Kevin Vavelin <kvavelin@geek-inc.fr>
2018-11-19 15:28:57 +0100

    MoodTracker

--- MoodTracker/app/src/main/java/geekinc/fr/moodtracker/MainActivity.kt
+++ MoodTracker/app/src/main/java/geekinc/fr/moodtracker/MainActivity.kt
@@ -1,12 +1,54 @@
 package geekinc.fr.moodtracker
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun selectMood(view: View) {
+        when (view.id) {
+            R.id.awesome_button -> {
+                val moodString = getString(R.string.mood_awesome)
+                val intent = Intent(this, MoodActivity::class.java).apply {
+                    putExtra("mood", moodString)
+                }
+                startActivity(intent)
+            }
+            R.id.cry_button -> {
+
+                    val moodString = getString(R.string.mood_cry)
+                    val intent = Intent(this, MoodActivity::class.java).apply {
+                        putExtra("mood", moodString)
+                    }
+                    startActivity(intent)
+
+            }
+            R.id.hurt_button -> {
+
+                    val moodString = getString(R.string.mood_hurt)
+                    val intent = Intent(this, MoodActivity::class.java).apply {
+                        putExtra("mood", moodString)
+                    }
+                    startActivity(intent)
+
+            }
+            R.id.happy_button -> {
+
+                    val moodString = getString(R.string.mood_happy)
+                    val intent = Intent(this, MoodActivity::class.java).apply {
+                        putExtra("mood", moodString)
+                    }
+                    startActivity(intent)
+
+            }
+        }
     }
 }
--- MoodTracker/app/src/main/java/geekinc/fr/moodtracker/MoodActivity.kt
+++ MoodTracker/app/src/main/java/geekinc/fr/moodtracker/MoodActivity.kt
@@ -2,11 +2,49 @@ package geekinc.fr.moodtracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.widget.Button
+import android.widget.TextView
 
 class MoodActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mood)
+
+        val moodString = intent.getStringExtra("mood")
+
+        val button = findViewById<Button>(R.id.mood_button).apply {
+            text = moodString
+        }
+
+        val rootView = button.rootView
+
+        when {
+            moodString == getString(R.string.mood_happy) -> {
+                findViewById<TextView>(R.id.mood_text_view).apply {
+                    text = getString(R.string.mood_happy_text)
+                }
+                rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.mood_happy))
+            }
+            moodString == getString(R.string.mood_cry) -> {
+                findViewById<TextView>(R.id.mood_text_view).apply {
+                    text = getString(R.string.mood_cry_text)
+                }
+                rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.mood_cry))
+            }
+            moodString == getString(R.string.mood_awesome) -> {
+                findViewById<TextView>(R.id.mood_text_view).apply {
+                    text = getString(R.string.mood_awesome_text)
+                }
+                rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.mood_awesome))
+            }
+            moodString == getString(R.string.mood_hurt) -> {
+                findViewById<TextView>(R.id.mood_text_view).apply {
+                    text = getString(R.string.mood_hurt_text)
+                }
+                rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.mood_hurt))
+            }
+        }
     }
 }
