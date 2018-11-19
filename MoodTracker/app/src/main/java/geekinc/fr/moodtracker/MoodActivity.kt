a22fbea63d81eb598bf8415e86b2f3054833a3c0
Kevin Vavelin <kvavelin@geek-inc.fr>
2018-11-19 15:27:33 +0100

    MoodTracker

---
+++ MoodTracker/app/src/main/java/geekinc/fr/moodtracker/MoodActivity.kt
@@ -0,0 +1,12 @@
+package geekinc.fr.moodtracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MoodActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mood)
+    }
+}
