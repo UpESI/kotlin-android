2c0ff4a2441ae817cff1721a8c8e4266f091912a
Julien Paris <pro.julienparis@gmail.com>
2018-11-19 15:42:07 +0100

    Julien MoodTracker v1

--- julien/MoodTracker/app/src/main/java/julien/fr/moodtracker/MainActivity.kt
+++ julien/MoodTracker/app/src/main/java/julien/fr/moodtracker/MainActivity.kt
@@ -1,12 +1,24 @@
 package julien.fr.moodtracker
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun selectMood (view: View) {
+        val intent: Intent = Intent(this, MoodActivity::class.java).apply {
+            putExtra("selectedMood", view.tag.toString())
+        }
+
+        println(view.tag.toString())
+
+        startActivity(intent)
     }
 }
